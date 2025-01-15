93e258af538c7b8952f473833be714aa497318fb
jgc777 <90793213+jgc777@users.noreply.github.com>
2025-01-15 20:21:14 +0100

    .

--- ghpagessimplified.js
+++ ghpagessimplified.js
@@ -1,26 +1,28 @@
 // Made by Jgc7 (https://github.com/jgc777)
-async function getReposbyUsername(username) { // Get the list with the repos of a user
-    username = username.toLowerCase();
-    if (username === "") {
-      console.warn(`No username provided!`);
-      return ["No username provided!"];
-    }
-    const response = await fetch(`https://api.github.com/users/${username}/repos`); // Fetch the repos
+async function getReposbyUsername(username) {
+  username = username.toLowerCase();
+  if (!username) {
+    console.warn(`No username provided!`);
+    return ["No username provided!"];
+  }
+  try {
+    const response = await fetch(`https://api.github.com/users/${username}/repos`);
     if (!response.ok) {
       console.warn(`Error fetching repos`);
       return ["Error fetching repos"];
     }
     const repos = await response.json();
-    let repoList = [];
-    repos.forEach(repo => {
-        if (repo.name.toLowerCase() === username || repo.name.toLowerCase() === `${username}.github.io`) return; // Skip the user's profile repo and github pages repo
-        repoList.push(repo);
-    });
-    return repoList;
+    return repos.filter(repo => repo.name.toLowerCase() !== username && repo.name.toLowerCase() !== `${username}.github.io`);
+  } catch (error) {
+    console.error(`Fetch error: ${error}`);
+    return ["Error fetching repos"];
+  }
 }
-async function appendRepos() { // Append the repos to a list, ordered by pinned and stars
-    let RepoList = await getReposbyUsername("{{ site.github.owner_name }}");
-    const repoListElement = document.getElementById('repo-list');
+
+async function appendRepos() {
+  const repoList = await getReposbyUsername("{{ site.github.owner_name }}");
+  const repoListElement = document.getElementById('repo-list');
+  if (repoListElement) {
     repoListElement.innerHTML = '';
     repoList.forEach(repo => {
       const listItem = document.createElement("li");
@@ -30,12 +32,14 @@ async function appendRepos() { // Append the repos to a list, ordered by pinned 
       listItem.appendChild(link);
       repoListElement.appendChild(listItem);
     });
+  }
 }
+
 let attempts = 0;
-function checkRepoList() {
-    console.log(`Appending the repo list (attempt #${++attempts})`);
-    const repoListElement = document.getElementById('repo-list');
-    if (repoListElement && repoListElement.children.length === 0) appendRepos();
-    else clearInterval(intervalId);
-}
-const intervalId = setInterval(checkRepoList, 1000);
+const intervalId = setInterval(() => {
+  console.log(`Appending the repo list (attempt #${++attempts})`);
+  const repoListElement = document.getElementById('repo-list');
+  if (repoListElement && repoListElement.children.length === 0) {
+    appendRepos().then(() => clearInterval(intervalId));
+  }
+}, 1000);
