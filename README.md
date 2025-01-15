a3ea37bbe40e02565991e38e02921284d914b2cf
jgc777 <90793213+jgc777@users.noreply.github.com>
2025-01-15 19:33:01 +0100

    .

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# GithubRepoAPI
+# GitHubRepoAPI
 Get the github repos of a user, with more options. It doesn't want to show all the repos if they are more than 30. I'm working on a new version to fix this.
 ## I know the appendRepos() works but I don't know if the API ones work.
 
--- demo/defaultapikey.js
+++ demo/defaultapikey.js
@@ -1,5 +1,5 @@
 const apiKey = "github_pat_".concat("11AVUWJ7I0tuimieFyMoqz_I80XwaPEIn7zJRVlHQRgtJ3DwKhhDpuHtYEREzfQAimFUOLFRIFy960aeM0"); // Define the token
 // I know this is not the way to store a token, but this is just a demo and it doesn't have any perms
 const script = document.createElement('script'); // Create a script element
-script.src = `https://jgc777.github.io/GithubRepoAPI/api.js?token=${apiKey}`; // Get the token defined up here and pass it to the script
+script.src = `https://jgc777.github.io/GitHubRepoAPI/api.js?token=${apiKey}`; // Get the token defined up here and pass it to the script
 document.head.appendChild(script); // Append the script to the html document
