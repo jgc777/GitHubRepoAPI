bb1cd01ef19401603ec2cd774980e83c2aefa21c
jgc777 <90793213+jgc777@users.noreply.github.com>
2025-01-16 18:53:05 +0100

    oops

--- demo/defaultapikey.js
+++ demo/defaultapikey.js
@@ -1,3 +1,4 @@
+// Made by Jgc7 (https://github.com/jgc777)
 const apiKey = "github_pat_".concat("11AVUWJ7I0tuimieFyMoqz_I80XwaPEIn7zJRVlHQRgtJ3DwKhhDpuHtYEREzfQAimFUOLFRIFy960aeM0"); // Define the token
 // I know this is not the way to store a token, but this is just a demo and it doesn't have any perms
 const script = document.createElement('script'); // Create a script element
